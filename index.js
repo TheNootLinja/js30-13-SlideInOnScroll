7fba89285df8866b4e3a30b392795882e3a9e2f0
TheNootLinja <thenootlinja@gmail.com>
2022-02-21 21:21:37 -0500

    Add code for final project

---
+++ index.js
@@ -0,0 +1,32 @@
+const slidingImages = document.querySelectorAll('.slide-in');
+
+const checkSlide = (e) => {
+  slidingImages.forEach(image => {
+    const slideInAt = (window.scrollY + window.innerHeight) - image.height/2;
+    const imageBottom = (image.offsetTop + image.height);
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if(isHalfShown && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  })
+}
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+window.addEventListener('scroll', debounce(checkSlide))
